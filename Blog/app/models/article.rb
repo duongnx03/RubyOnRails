f81f1c3bfe6aa339e6b2a940f1aae89180cf3f22
DuongNX <duongnx170803@gmail.com>
2023-12-06 19:58:51 +0700

    Create a comment section for the blog

--- Blog/app/models/article.rb
+++ Blog/app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   include Visible
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :title, presence: true, length: {minimum: 3}
   validates :body, presence: true, length: {minimum: 10}
